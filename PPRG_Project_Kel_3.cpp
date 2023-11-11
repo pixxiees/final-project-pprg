b9daba457eedef9527d575e5e523413096b9fdda
pixxiees <86036955+pixxiees@users.noreply.github.com>
2023-11-11 22:32:01 +0700

    Update PPRG_Project_Kel_3.cpp

--- PPRG_Project_Kel_3.cpp
+++ PPRG_Project_Kel_3.cpp
@@ -3,7 +3,8 @@
 #include <conio.h>
 using namespace std;
 
-struct siswa {
+struct siswa
+{
     string nama;
     float nilai_mtk;
     float nilai_bindo;
@@ -13,11 +14,15 @@ struct siswa {
 
 siswa biodata[1000];
 
-void sort_siswa(int jumlah_pendaftar) {
-    for(int i=1; i<jumlah_pendaftar; i++) {
+void sort_siswa(int jumlah_pendaftar)
+{
+    for (int i = 1; i < jumlah_pendaftar; i++)
+    {
         int temp = i;
-        for(int j=i+1; j<=jumlah_pendaftar; j++) {
-            if(biodata[temp].rerata<biodata[j].rerata) {
+        for (int j = i + 1; j <= jumlah_pendaftar; j++)
+        {
+            if (biodata[temp].rerata < biodata[j].rerata)
+            {
                 temp = j;
             }
         }
@@ -25,31 +30,48 @@ void sort_siswa(int jumlah_pendaftar) {
     }
 }
 
-int main() {
+void data_siswa(int initial, int jumlah_kuota)
+{
+    for (int i = initial; i <= jumlah_kuota; i++)
+    {
+        this_thread::sleep_for(chrono::milliseconds(80));
+        cout << i << ". " << biodata[i].nama << " (" << biodata[i].rerata << ")" << endl;
+    }
+    cout << endl
+         << endl;
+}
+
+int main()
+{
     int jumlah_pendaftar;
     int jumlah_kuota;
     string s = "-----SELAMAT DATANG PADA LAMAN REGISTRASI SISWA BARU TAHUN AJARAN 2024/2025-----";
 
     system("CLS");
     cout << "--------------------------------------------------------------------------------" << endl;
-    for(int i=0; i<s.length(); i++) {
+    for (int i = 0; i < s.length(); i++)
+    {
         this_thread::sleep_for(chrono::milliseconds(80));
         cout << s[i];
     }
     cout << endl;
-    cout << "--------------------------------------------------------------------------------" << endl << endl;
-    
+    cout << "--------------------------------------------------------------------------------" << endl
+         << endl;
+
     cout << "Masukkan jumlah pendaftar: ";
     cin >> jumlah_pendaftar;
     cout << "Masukkan jumlah kuota: ";
     cin >> jumlah_kuota;
-    cout << endl << endl;
+    cout << endl
+         << endl;
 
     system("CLS");
     cout << "--------------------------------------------------------------------------------" << endl;
     cout << "------------------------------Data siswa pendaftar------------------------------" << endl;
-    cout << "--------------------------------------------------------------------------------" << endl << endl;
-    for(int i=1; i<=jumlah_pendaftar; i++) {
+    cout << "--------------------------------------------------------------------------------" << endl
+         << endl;
+    for (int i = 1; i <= jumlah_pendaftar; i++)
+    {
         cout << "Nama pendaftar ke-" << i << setw(8) << ": ";
         cin.ignore();
         getline(cin, biodata[i].nama);
@@ -57,20 +79,62 @@ int main() {
         cin >> biodata[i].nilai_mtk;
         cout << "Nilai bahasa Indonesia" << setw(5) << ": ";
         cin >> biodata[i].nilai_bindo;
-        biodata[i].rerata = (biodata[i].nilai_bindo+biodata[i].nilai_mtk)/2;
+        biodata[i].rerata = (biodata[i].nilai_bindo + biodata[i].nilai_mtk) / 2;
         cout << "Nilai rerata " << setw(14) << ": " << biodata[i].rerata;
-        cout << endl << endl;
+        cout << endl
+             << endl;
     }
+
+    system("CLS");
+    int menu;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    cout << "------------------------Pilih halaman yang ingin dituju-------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl
+         << endl;
+    cout << "1. Daftar siswa diterima." << endl;
+    cout << "2. Daftar siswa tidak diterima." << endl;
+    cout << "3. Cari status pendaftar berdasarkan nama." << endl
+         << endl;
+    cout << "Menu dipilih: ";
+    cin >> menu;
 
     sort_siswa(jumlah_pendaftar);
 
     system("CLS");
-    cout << "--------------------------------------------------------------------------------" << endl;
-    cout << "------------------------------Daftar siswa diterima-----------------------------" << endl;
-    cout << "--------------------------------------------------------------------------------" << endl << endl;
-    for(int i=1; i<=jumlah_kuota; i++) {
-        this_thread::sleep_for(chrono::milliseconds(80));
-        cout << i << ". " << biodata[i].nama << " (" << biodata[i].rerata << ")" << endl;
+    if (menu == 1)
+    {
+        cout << "--------------------------------------------------------------------------------" << endl;
+        cout << "------------------------------Daftar siswa diterima-----------------------------" << endl;
+        cout << "--------------------------------------------------------------------------------" << endl
+             << endl;
+        data_siswa(1, jumlah_kuota);
     }
-    cout << endl << endl;
+    else if (menu == 2)
+    {
+        cout << "--------------------------------------------------------------------------------" << endl;
+        cout << "---------------------------Daftar siswa tidak diterima--------------------------" << endl;
+        cout << "--------------------------------------------------------------------------------" << endl
+             << endl;
+        data_siswa(jumlah_kuota + 1, jumlah_pendaftar);
+    }
+    else if (menu == 3)
+    {
+        string pendaftar;
+        bool diterima = false;
+        cout << "Nama pendaftar: ";
+        cin.ignore();
+        getline(cin, pendaftar);
+        for (int i = 1; i <= jumlah_pendaftar; i++)
+        {
+            if (biodata[i].nama == pendaftar)
+            {
+                diterima = true;
+                break;
+            }
+        }
+        cout << "Pendaftar atas nama " << pendaftar << " dinyatakan ";
+        cout << (diterima ? "diterima." : "tidak diterima.") << endl
+             << endl;
+    }
+    return 0;
 }
